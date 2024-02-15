9b479dc4e68600cbe5a709ed6b248d2961f758a7
richard1 <richard@bomding.com>
2024-02-15 17:40:48 +0800

    feat: player shooting

--- Assets/Scripts/MySpaceInvader/Controller/PlayerController.cs
+++ Assets/Scripts/MySpaceInvader/Controller/PlayerController.cs
@@ -4,21 +4,26 @@ namespace MySpaceInvader.Controller
 {
     public class PlayerController : MonoBehaviour
     {
+        public Camera mainCamera;
         public GameObject bulletPrefab;
         public float speed = 10.0f;
         private float ammo = 30; // Starting ammo
+        private Vector3 _bulletOffset = new Vector3(0, 0.5f, 0);
 
         void Update()
         {
             // Player movement
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector3(mousePosition.x, transform.position.y, 0);
 
             // Shooting
             if (Input.GetButtonDown("Fire1") && ammo > 0)
             {
-                Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                var bullet = Instantiate(bulletPrefab, transform.position + _bulletOffset, Quaternion.identity);
+                bullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 100));
                 ammo--;
+                
+                
                 UpdateAmmoDisplay();
             }
         }
